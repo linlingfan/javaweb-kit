0da11b58ba7aff434c7b8a8f5190cf9e440ac99b
glinlfzy <glinlf.f@thelook.cn>
2019-02-16 13:20:22 +0800

    新增uniVocity-parsers csv解析包 感觉不错！

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ some tools  about javaweb
 
 1. springboot 整合 websocket
 2. 接口自定义访问权限 工具类 AuthKit RestUtils
-3. 读取CSV文件工具类 CSVKit
+3. 读取CSV文件工具类 CSVKit 
+
+   [jar 推荐：uniVocity-parsers：速度最快功能最全的CSV开发库之一](http://hao.jobbole.com/univocity-parsers/)
+   
 4. 日期格式化等工具类 DateKit
 5. Id生成 IdKit
 6. Json工具类jackJson JsonHelper
@@ -15,3 +18,13 @@ some tools  about javaweb
 12. Okhttp相关工具类 OkhttpKit
 13. Swagger2相关配置
 14. 链式集合类等
+
+### 项目经历
+    注意：对接第三方活动。虚拟商城？
+         退换货设计？
+         消息推送设计
+         定时任务设计
+         系统拆分设计
+         数据同步设计（公众号用户信息）设计多线程，springboot的异步调用 ，java8 的并行流
+         
+            
--- src/test/com/glinlf/growth/kit/CsvKitTest.java
+++ src/test/com/glinlf/growth/kit/CsvKitTest.java
@@ -2,9 +2,16 @@ package com.glinlf.growth.kit;
 
 import com.glinlf.growth.NoRollbackTester;
 import com.glinlf.growth.utils.CSVKit;
+import com.univocity.parsers.common.processor.ColumnProcessor;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -29,5 +36,48 @@ public class CsvKitTest extends NoRollbackTester {
 
         List list = CSVKit.readerCVSListByClass(url);
         System.out.println(list);
+    }
+
+    public Reader getReader(String relativePath) throws UnsupportedEncodingException {
+        return new InputStreamReader(this.getClass().getResourceAsStream(relativePath), "UTF-8");
+    }
+
+    /**
+     * 使用 uniVocity-parsers
+     * 按行读取
+     * 也可以映射成自定义定义的对象
+     *
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void testCsvParserByRow() throws UnsupportedEncodingException {
+
+        CsvParserSettings settings = new CsvParserSettings();
+        // mac /n 换行
+        settings.getFormat().setLineSeparator("\n");
+        // 创建CSV解析器
+        CsvParser parser = new CsvParser(settings);
+        // 一行语句处理所有行
+        List<String[]> allRows = parser.parseAll(getReader("/data/list.csv"));
+        System.out.println(allRows);
+//        System.out.println(Arrays.toString(allRows.get(0)));
+    }
+
+    @Test
+    public void testCsvParserByColumn() throws UnsupportedEncodingException {
+
+        CsvParserSettings parserSettings = new CsvParserSettings();
+        parserSettings.getFormat().setLineSeparator("\n");
+        parserSettings.setHeaderExtractionEnabled(true);
+        // 使用列处理器读取所有列的值
+        ColumnProcessor rowProcessor = new ColumnProcessor();
+        parserSettings.setRowProcessor(rowProcessor);
+        CsvParser parser = new CsvParser(parserSettings);
+        // 送入列处理器
+        parser.parse(getReader("/data/list.csv"));
+        Map<String, List<String>> columnValues = rowProcessor.getColumnValuesAsMapOfNames();
+        System.out.println(columnValues);
+        List<String> columns = rowProcessor.getColumn(0);
+        System.out.println(columns);
     }
 }
